954708a6265e2dfa65bf11baa9ea6af0c9693d15
Lesvin Josue <lardon99@.com>
2022-03-14 17:50:45 -0600

    cambios por Lesvin

--- AppProyectoMovil/App.js
+++ AppProyectoMovil/App.js
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
-import AgregarCliente from './SRC/Componentes/login';
+import AgregarCliente from './SRC/Componentes/AgregarClientes';
+import Login from './SRC/Componentes/login';
 export default function App() {
   return (
     <AgregarCliente></AgregarCliente>
