466426940743b2f0b526217e64ea4a3be24f6384
Lesvin Josue <lardon99@.com>
2022-03-14 18:12:39 -0600

    xd

--- AppProyectoMovil/App.js
+++ AppProyectoMovil/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import Navigation from "./SRC/Navigation/Navigation";
 
-<<<<<<< HEAD
 import AgregarCliente from './SRC/Componentes/AgregarClientes';
 import Login from './SRC/Componentes/login';
-=======
->>>>>>> 76e1e5d4d45122d2dd497c154bb078f626c02a28
+
 export default function App() {
   return (
-    <Navigation/>
+    <AgregarCliente></AgregarCliente>
   );
 }
