40f8e850c9b471f8e917bf963dcf4a16c3bc28e3
KevinM1466 <kevd_hernandezm@unicah.edu>
2022-03-16 02:35:59 -0600

    Ok K.H

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Navigation from "./src/Navigation/Navigation";
+import RootStack from './src/Navigation/RootStack'
+
+export default function App() {
+  return (
+    <RootStack/>
+  );
+}
